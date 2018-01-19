6b964aead57b32b7003521ca291362d28f161f99
Miro Dojkic <mdojkic@gmail.com>
2018-01-19 16:41:03 +0100

    force async load of second font

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,13 +4,14 @@ import { AppContainer } from 'react-hot-loader';
 import Loader from '../Loader';
 import Monthly from '../Monthly';
 import StorageProvider from '../Storage';
+import './font.css';
 
 /* eslint-disable no-unused-expressions */
 injectGlobal`
 * {
   box-sizing: border-box;
-  font-family: "Roboto", sans-serif;
   font-size: 16px;
+  font-family: "Roboto", sans-serif;
 }
 `;
 /* eslint-enable no-unusued-expressions */
