fb8ab0f65bea2355588569884adf05106431edfb
Miro Dojkic <mdojkic@gmail.com>
2018-02-08 02:02:45 +0100

    :recycle: move transaction methods to model

--- src/models/transaction.js
+++ src/models/transaction.js
@@ -1,10 +1,21 @@
 import * as L from 'partial.lenses';
-import { isAppliedToMonth } from '../util/transactions';
+import { truncateToMonth } from '../util/datetimes';
+
+const isCreatedAtMonth = (transaction, date) =>
+  truncateToMonth(new Date(transaction.created_at)) === truncateToMonth(date);
+
+const isRepeatedOverMonth = (transaction, date) =>
+  transaction.repeat === 'monthly' &&
+  new Date(transaction.created_at) <= date &&
+  new Date(transaction.repeat_until) >= date;
+
+const isAppliedToMonth = date => transaction =>
+  isCreatedAtMonth(transaction, date) || isRepeatedOverMonth(transaction, date);
 
 const Transaction = {
   Type: { income: 'income', expense: 'expense' },
 
-  byMonth: date => L.filter(transaction => isAppliedToMonth(transaction, date)),
+  byMonth: date => L.filter(isAppliedToMonth(date)),
 
   byType: type => L.filter(L.get(['type', L.is(type)])),
 
